133f9c180a0c17ca517c8c45ad1ecd0eac7b23d4
fred.burgess <fred.burgess@whitehat.org.uk>
2019-04-07 11:40:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-  def display_board
+def display_board
     puts "A Tic Tac Toe board"
     puts "   |   |   "
     puts "-----------"
     puts "   |   |   "
     puts "-----------"
     puts "   |   |   "
-  end
+end
   
-  display_board
+display_board
   
